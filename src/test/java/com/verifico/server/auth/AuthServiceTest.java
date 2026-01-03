ce802c23fcc46bc83d8db20b3767b05a04a3c560
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-01-03 20:49:52 +1100

    to do: quick register endpoint unit tests

--- src/test/java/com/verifico/server/auth/AuthServiceTest.java
+++ src/test/java/com/verifico/server/auth/AuthServiceTest.java
@@ -1,6 +1,38 @@
 package com.verifico.server.auth;
 
-// unit tests for auth endpoints:
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.verifico.server.user.UserRepository;
+
+// unit tests for auth endpoints(happy path,duplicates password hashing):
+@ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
-  
+
+  @Mock
+  UserRepository userRepository;
+
+  @Mock
+  PasswordEncoder passwordEncoder;
+
+  @InjectMocks
+  AuthService authService;
+
+  @Test
+  void registerHappyPath() {
+  }
+
+  @Test
+  void duplicateEmail(){}
+
+  @Test
+  void duplicateUsername(){}
+
+  @Test
+  void checkForPassHashing(){}
 }
