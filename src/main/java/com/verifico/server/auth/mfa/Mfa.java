deca95c9321684ab52a186a45c3da31b8026b9e6
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-03-04 23:12:55 +1100

    feat: initialise MFA entitiy w/hashedCode,user,used,expiry,attempts

---
+++ src/main/java/com/verifico/server/auth/mfa/Mfa.java
@@ -0,0 +1,55 @@
+package com.verifico.server.auth.mfa;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.security.autoconfigure.SecurityProperties.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "mfa", indexes = {
+    @Index(name = "mfa_token", columnList = "mfaCode"),
+    @Index(name = "mfa_token_usr_id", columnList = "user_id")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+public class Mfa {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String mfaCode;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(nullable = false)
+  private boolean used;
+
+  @Column(nullable = false)
+  private LocalDateTime expiresAt;
+
+  @Column(nullable = false)
+  private int attempts = 0;
+
+  public boolean isMfaTokenExpired() {
+    return expiresAt.isBefore(LocalDateTime.now());
+  }
+
+}
