b11789e9a6f00709f8a09d12a76b7959696fe033
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-03-05 23:05:58 +1100

    feat: Implement functions for mfaRepo

--- src/main/java/com/verifico/server/auth/mfa/MfaRepository.java
+++ src/main/java/com/verifico/server/auth/mfa/MfaRepository.java
@@ -1,7 +1,20 @@
 package com.verifico.server.auth.mfa;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.transaction.Transactional;
 
 public interface MfaRepository extends JpaRepository<Mfa, Long> {
+  Optional<Mfa> findTopByUserIdAndUsedFalseAndExpiresAtAfterOrderByIdDesc(Long userId, LocalDateTime now);
 
+  @Transactional
+  @Modifying
+  @Query("UPDATE Mfa m SET m.used = true WHERE m.user.id = :userId AND m.used = false")
+  void invalidateAllActiveForUser(@Param("userId") Long userId);
 }
