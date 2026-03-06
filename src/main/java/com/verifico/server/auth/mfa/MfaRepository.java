be9efa3232b5b6e171447de5f44e6f304967a9b4
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-03-07 10:57:17 +1100

    feat: Implement new deleteAllByExpiresAtBefore + use findTopByUser_IdAndUsedFalseAndExpiresAtAfterOrderByIdDesc to match Mfa entitiy

--- src/main/java/com/verifico/server/auth/mfa/MfaRepository.java
+++ src/main/java/com/verifico/server/auth/mfa/MfaRepository.java
@@ -11,10 +11,14 @@ import org.springframework.data.repository.query.Param;
 import jakarta.transaction.Transactional;
 
 public interface MfaRepository extends JpaRepository<Mfa, Long> {
-  Optional<Mfa> findTopByUserIdAndUsedFalseAndExpiresAtAfterOrderByIdDesc(Long userId, LocalDateTime now);
+  Optional<Mfa> findTopByUser_IdAndUsedFalseAndExpiresAtAfterOrderByIdDesc(Long userId, LocalDateTime now);
 
   @Transactional
   @Modifying
   @Query("UPDATE Mfa m SET m.used = true WHERE m.user.id = :userId AND m.used = false")
   void invalidateAllActiveForUser(@Param("userId") Long userId);
+
+  // used in cleanup scheduler
+  void deleteAllByExpiresAtBefore(LocalDateTime now);
+
 }
