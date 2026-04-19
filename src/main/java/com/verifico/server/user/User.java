ea5bf7d3293b695f5e462846682b69ddd9f30922
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-04-19 21:59:05 +1000

    fix: changes made to User entity to support OAUTH login

--- src/main/java/com/verifico/server/user/User.java
+++ src/main/java/com/verifico/server/user/User.java
@@ -43,7 +43,9 @@ public class User {
   @Column(nullable = false, unique = true)
   private String email;
 
-  @Column(nullable = false)
+  // This is made true as OAUTH users will not have passwords...
+  // Enforce password validity for users manually signup.
+  @Column(nullable = true)
   @JsonIgnore
   private String password;
 
@@ -61,5 +63,42 @@ public class User {
 
   @Column(name = "joined_date", nullable = false, updatable = false)
   private LocalDate joinedDate = LocalDate.now();
+
+  // OAUTH fields:
+  @Column(name = "auth_provider")
+  private String authProvider; // i.e. google/github & null for non-oauth users
+
+  @Column(name = "provided_user_id")
+  private String providerUserId;
+
+  public static User createOAuthUser(String email, String name, String profilePicture, String provider,
+      String providerId) {
+    User user = new User();
+
+    user.setEmail(email);
+    user.setUsername(createUserNameFromNameForOauthUsers(name));
+    String[] parts = name != null ? name.split(" ", 2) : new String[] { "User", "" };
+    user.setFirstName(parts[0]);
+    user.setLastName(parts.length > 1 ? parts[1] : "");
+    user.setPassword(null);
+    user.setAuthProvider(provider);
+    user.setProviderUserId(providerId);
+    user.setBio("");
+    user.setAvatarUrl(profilePicture);
+    return user;
+  }
+
+  // link new OAuth provider to existing email/password account:
+  public void linkOAuthProvider(String provider, String providerUserId){
+    this.authProvider = provider;
+    this.providerUserId = providerUserId;
+  }
+
+  // unqiue username from oauth helper function:
+  // based on the single string for full name
+  // we make a unique username.
+  public static String createUserNameFromNameForOauthUsers(String name) {
+    return "";
+  }
 
 }
